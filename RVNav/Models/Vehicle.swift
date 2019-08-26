286311dbdff18e909e5c080fa103eca91ce9834d
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-08-26 16:16:14 -0400

    Added Vehicle Model, Storyboarded Vehicle Settings.

---
+++ RVNav/Models/Vehicle.swift
@@ -0,0 +1,35 @@
+//
+//  Vehicle.swift
+//  RVNav
+//
+//  Created by Ryan Murphy on 8/26/19.
+//  Copyright © 2019 RVNav. All rights reserved.
+//
+
+import Foundation
+
+class Vehicle: Codable {
+    
+    let height: Float?
+    let weight: Float?
+    let width: Float?
+    let length: Float?
+    let axelCount: Int?
+    let vehicleClass: String?
+    let dualTires: Bool?
+    let trailer: Bool?
+    
+    init(height: Float?, weight: Float?, width: Float?, length: Float?, axelCount: Int?, vehicleClass: String?, dualTires: Bool?, trailer: Bool?) {
+        
+        self.height = height
+        self.weight = weight
+        self.width = width
+        self.length = length
+        self.axelCount = axelCount
+        self.vehicleClass = vehicleClass
+        self.dualTires = dualTires
+        self.trailer = trailer
+    
+    }
+    
+}
---
+++ RVNav/ViewControllers/VehicleFormViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VehicleFormViewController.swift
+//  RVNav
+//
+//  Created by Ryan Murphy on 8/26/19.
+//  Copyright © 2019 RVNav. All rights reserved.
+//
+
+import UIKit
+
+class VehicleFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
