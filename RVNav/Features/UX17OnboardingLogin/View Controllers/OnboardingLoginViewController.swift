98d955cff554e28c8b2fa80da37cb8847bc20cb5
Joshua Sharp <sharp.josh@gmail.com>
2019-12-16 17:18:48 -0500

    Initial Storyboards created

---
+++ RVNav/Features/UX17OnboardingLogin/View Controllers/OnboardingLoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingLoginViewController.swift
+//  RVNav
+//
+//  Created by Lambda_School_Loaner_214 on 12/16/19.
+//  Copyright © 2019 RVNav. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
