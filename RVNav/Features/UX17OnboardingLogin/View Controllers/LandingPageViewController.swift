ef17e0e82e85ea836fe1a36d9f6c1c31b295ce7d
Jake <jlconnerly@gmail.com>
2019-12-17 17:28:05 -0600

    Landing Page ScrollView Operational

---
+++ RVNav/Features/UX17OnboardingLogin/View Controllers/LandingPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LandingPageViewController.swift
+//  RVNav
+//
+//  Created by Jake Connerly on 12/17/19.
+//  Copyright © 2019 RVNav. All rights reserved.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
