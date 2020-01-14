9ab28d5d1cd4187c9ff890fa83412a507cd31497
Jake <jlconnerly@gmail.com>
2020-01-13 20:10:34 -0600

    RVTypeTextfield now presents pickerView

--- RVNav/AddVehicle/Storyboards/AddVehicleViewController.swift
+++ RVNav/AddVehicle/Storyboards/AddVehicleViewController.swift
@@ -9,14 +9,45 @@
 import UIKit
 
 class AddVehicleViewController: ShiftableViewController {
+    
+    // MARK: - IBOutlets
 
+    @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var vehicleNameTextField: UITextField!
+    @IBOutlet weak var heightFeetTextField: UITextField!
+    @IBOutlet weak var heightInchesTextField: UITextField!
+    @IBOutlet weak var widthFeetTextField: UITextField!
+    @IBOutlet weak var widthInchesTextField: UITextField!
+    @IBOutlet weak var lengthFeetTextField: UITextField!
+    @IBOutlet weak var lengthInchesTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var axelCountTextField: UITextField!
+    @IBOutlet weak var duelWheelSwitch: UISwitch!
+    @IBOutlet weak var rvTypeTextField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    // MARK: - View LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if #available(iOS 13.0, *) {
             overrideUserInterfaceStyle = .light
         }
+        buttonUISetup()
     }
     
+    private func buttonUISetup() {
+        //cancel button
+        cancelButton.layer.borderWidth = 0.4
+        cancelButton.layer.borderColor = UIColor.babyBlue.cgColor
+        cancelButton.layer.cornerRadius = 4
+        //add button
+        addButton.layer.borderWidth = 0.4
+        addButton.layer.borderColor = UIColor.babyBlue.cgColor
+        addButton.layer.cornerRadius = 4
+    }
 
     /*
     // MARK: - Navigation
@@ -27,5 +58,13 @@ class AddVehicleViewController: ShiftableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+   override func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == rvTypeTextField {
+            view.endEditing(true)
+            performSegue(withIdentifier: "ShowRVTypesSegue", sender: self)
+        }
+    }
+    
 }
+
