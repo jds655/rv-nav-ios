2de4a6069e64f45af6a5506de7e3f2c55cbab1c8
Joshua Sharp <sharp.josh@gmail.com>
2020-02-06 00:43:47 -0500

    Fixed bug in choosing map style
    choosing a different map type was clearing the route, fixed that.  Map route does still clear on a new route selection.

--- RVNav/MapView/View Controllers/MapViewController.swift
+++ RVNav/MapView/View Controllers/MapViewController.swift
@@ -23,19 +23,23 @@ class MapViewController: UIViewController, AGSGeoViewTouchDelegate {
     
     private var mapType: AGSBasemapType = .navigationVector {
         didSet{
-            guard let location = mapView.locationDisplay.location,
-                let lat = location.position?.y,
-                let lon = location.position?.x else { return }
-            if let route = directionsController.mapAPIController.selectedRoute,
-                let routeGeometry = route.routeGeometry {
-                let count = self.mapView.graphicsOverlays.count
-                print("Graphic Overlay count: \(count)")
-                if count == 1 {
-                    self.mapView.graphicsOverlays.removeAllObjects()
-                }
-                self.mapView.setViewpointGeometry(routeGeometry, padding: 100, completion: nil)
+            var map: AGSMap
+            switch self.mapType {
+            case .navigationVector:
+                map = AGSMap(basemap: AGSBasemap.navigationVector())
+            case .imageryWithLabelsVector:
+                map = AGSMap(basemap: AGSBasemap.imageryWithLabelsVector())
+            case .terrainWithLabelsVector:
+                map = AGSMap(basemap: AGSBasemap.terrainWithLabelsVector())
+            default:
+                return
             }
-            mapView.map = AGSMap(basemapType: mapType, latitude: lat, longitude: lon, levelOfDetail: 18)
+            DispatchQueue.main.async {
+                self.mapView.map = map
+                guard let route = self.directionsController.mapAPIController.selectedRoute,
+                    let routeGeometry = route.routeGeometry else { return }
+                    self.mapView.setViewpointGeometry(routeGeometry, padding: 100, completion: nil)
+            }
         }
     }
     
@@ -47,7 +51,9 @@ class MapViewController: UIViewController, AGSGeoViewTouchDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         Analytics.logEvent("app_opened", parameters: nil)
+        //Listen out for a notification that a route was selected and triggers mapRoute
         NotificationCenter.default.addObserver(self, selector: #selector(mapRoute(notification:)), name: .routeNeedsMapped, object: nil)
+        //Initial map/mapView setup
         setupMap()
     }
     
@@ -58,9 +64,12 @@ class MapViewController: UIViewController, AGSGeoViewTouchDelegate {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        //Check if this is the first launch and send to LaunchPage if so
         if UserDefaults.isFirstLaunch() {
             performSegue(withIdentifier: "LandingPageSegue", sender: self)
-        } else if KeychainWrapper.standard.string(forKey: "accessToken") == nil && !UserDefaults.isFirstLaunch() {
+        } else if
+            //check if we have a stored API token (logged in)
+            KeychainWrapper.standard.string(forKey: "accessToken") == nil && !UserDefaults.isFirstLaunch() {
             performSegue(withIdentifier: "SignInSegue", sender: self)
         }
     }
@@ -98,21 +107,38 @@ class MapViewController: UIViewController, AGSGeoViewTouchDelegate {
     }
     
     @objc public func mapRoute(notification: NSNotification) {
-        guard let route = directionsController.mapAPIController.selectedRoute else { return }
+        //Check if we have a route selected by user
+        guard let route = directionsController.mapAPIController.selectedRoute else
+        { //if not show map at current location
+            guard let location = mapView.locationDisplay.location,
+                let lat = location.position?.y,
+                let lon = location.position?.x
+                else { return }
+            DispatchQueue.main.async {
+                self.mapView.map = AGSMap(basemapType: self.mapType, latitude: lat, longitude: lon, levelOfDetail: 18)
+            }
+            return
+                
+        }
+        //If we DO have a route display it
         DispatchQueue.main.async {
-            if let routePolyline = route.routeGeometry {
+            if let routeGeometry = route.routeGeometry {
+                //Draw the route in an overlay and add it, clearing any previous
                 let routeSymbol = AGSSimpleLineSymbol(style: .solid, color: .blue, width: 4)
-                let routeGraphic = AGSGraphic(geometry: routePolyline, symbol: routeSymbol, attributes: nil)
+                let routeGraphic = AGSGraphic(geometry: routeGeometry, symbol: routeSymbol, attributes: nil)
                 let count = self.graphicsOverlay.graphics.count
                 print("Graphic Overlay count: \(count)")
                 self.graphicsOverlay.graphics.removeAllObjects()
                 self.graphicsOverlay.graphics.add(routeGraphic)
+                
+                //Mark start and stop points
+                guard let start = route.stops.first?.geometry,
+                    let end = route.stops.last?.geometry else { return }
+                self.addMapMarker(location: start, style: .diamond, fillColor: .green, outlineColor: .black)
+                self.addMapMarker(location: end, style: .X, fillColor: .red, outlineColor: .red)
+                //Set the map scale with padding
+            self.mapView.setViewpointGeometry(routeGeometry, padding: 100, completion: nil)
             }
-            guard let start = route.stops.first?.geometry,
-                let end = route.stops.last?.geometry else { return }
-            self.addMapMarker(location: start, style: .diamond, fillColor: .green, outlineColor: .black)
-            self.addMapMarker(location: end, style: .X, fillColor: .red, outlineColor: .red)
-            self.mapView.setViewpointGeometry(route.routeGeometry!, padding: 100, completion: nil)
         }
         ARSLineProgress.hide()
     }
@@ -122,18 +148,22 @@ class MapViewController: UIViewController, AGSGeoViewTouchDelegate {
     // Creates a new instance of AGSMap and sets it to the mapView.
     private func setupMap() {
         mapView.locationDisplay.autoPanMode = .recenter
+        //Start location services
         mapView.locationDisplay.start {error in
             DispatchQueue.main.async {
                 if let error = error {
+                    //If location services fails to work, just pick a point to start with
                     NSLog("ERROR: Error starting AGSLocationDisplay: \(error)")
                     self.mapView.map = AGSMap(basemapType: self.mapType, latitude: 40.615518, longitude: -74.026005, levelOfDetail: 0)
                 } else {
+                    //Else, set map to our current location
                     if let location = self.mapView.locationDisplay.location,
                         let lat = location.position?.y ,
                         let lon = location.position?.x {
                         
                         self.mapView.map = AGSMap(basemapType: self.mapType, latitude: lat, longitude: lon, levelOfDetail: 0)
                     } else {
+                        //If No error occured but no location returned, pick a point to start
                         self.mapView.map = AGSMap(basemapType: self.mapType, latitude: 40.615518, longitude: -74.026005, levelOfDetail: 0)
                     }
                 }
@@ -141,11 +171,11 @@ class MapViewController: UIViewController, AGSGeoViewTouchDelegate {
         }
         mapView.touchDelegate = self
         let count = mapView.graphicsOverlays.count
-//        print("Graphic Overlay count: \(count)")
-//        if count == 0 {
-//        }else {
-//            mapView.graphicsOverlays.removeAllObjects()
-//        }
+        print("Graphic Overlay count: \(count)")
+        if count == 0 {
+        }else {
+            mapView.graphicsOverlays.removeAllObjects()
+        }
         mapView.graphicsOverlays.add(graphicsOverlay)
     }
     
